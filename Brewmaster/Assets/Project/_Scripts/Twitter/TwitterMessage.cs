306592e4894baf812b4f41c998fd637f87d5acbe
NoodTheGD <noodthegd@gmail.com>
2024-08-29 22:20:21 +0700

    update new message

--- Brewmaster/Assets/Project/_Scripts/Twitter/TwitterMessage.cs
+++ Brewmaster/Assets/Project/_Scripts/Twitter/TwitterMessage.cs
@@ -8,7 +8,7 @@ namespace Game
 	{
 		public static string GetTwitterMessage()
 		{
-			return $"StarkArcade challenge ----- {PlayerData.TotalPoint} -----";
+			return $"Just brewed up a storm in Brewmaster 🍺\nScored {PlayerData.TotalPoint} points and 5 STRK for my skills——what a game!\nCheck out what @BoltwadeStudio has been cooking up on @Starknet\nIf you're not in on this yet, you're missing out! 👀";
 		}
 	}
 }
