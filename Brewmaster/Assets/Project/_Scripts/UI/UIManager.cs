339b271dd5d65b95552b0d6b66ee9457d5440f6d
meow-bland <87884396+doraemonidol@users.noreply.github.com>
2024-09-19 20:48:17 +0700

    fix: update treasury when shopping

--- Brewmaster/Assets/Project/_Scripts/UI/UIManager.cs
+++ Brewmaster/Assets/Project/_Scripts/UI/UIManager.cs
@@ -52,8 +52,9 @@ namespace Game
 			_pResumeBtn.OnClick += OnResumeClickHandler;
 			_storeConfirmBtn.OnClick += () =>
 			{
-				GameEvent.Instance.OnNextDay?.Invoke();
+				// GameEvent.Instance.OnNextDay?.Invoke();
 				HideStoreMenu();
+				_endDayPanel.Show(false);
 				_isStorePhrase = false;
 			};
 
